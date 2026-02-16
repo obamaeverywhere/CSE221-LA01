4257f61307bf4178fb6a4271e5a3588a4d947ee4
Maheer Tajwar <maheertajwar@gmail.com>
2026-02-16 10:34:39 +0600

    added 2

--- E_Reverse_Sorting.cpp
+++ E_Reverse_Sorting.cpp
@@ -1,84 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     int n;
-    cin >> n;
-    int arr[n];
+    cin>>n;
 
-    for (int i = 0; i < n; i++)
-    {
+    int arr[n];
+    vector<int> arr2;
+
+    for (int i = 0; i < n;i++) {
         cin >> arr[i];
     }
 
-    bool hasDuplicate = false;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-        {
-            if (arr[i] == arr[j])
-            {
-                hasDuplicate = true;
+    bool sorted = true;
+    bool repeated = true;
+    while(repeated) {
+        repeated = false;
+        for (int i = 0; i < n-2;i++) {
+            if(arr[i+2]<arr[i]) {
+                int temp = arr[i + 2];
+                arr[i + 2] = arr[i];
+                arr[i] = temp;
+                arr2.push_back(i+1);
+                repeated = true;
+            }
+        }
+    }
+    for (int i = 0; i < n-1;i++) {
+        for (int j = i + 1; j < n;j++) {
+            if(arr[i]>arr[j]) {
+                sorted = false;
                 break;
             }
         }
-        if (hasDuplicate)
-            break;
     }
-
-    long long inv = 0;
-    for (int i = 0; i < n; i++)
-        for (int j = i + 1; j < n; j++)
-        {
-            if (arr[i] > arr[j])
-                inv++;
-        }
-
-    if (!hasDuplicate && inv % 2 == 1)
-    {
-        cout << "NO\n";
+    if(!sorted) {
+        cout << "NO" << endl;
         return 0;
     }
-
-    vector<pair<int, int>> op;
-    for (int i = 0; i < n; i++)
-    {
-        int pos = i;
-        for (int j = i; j < n; j++)
-        {
-            if (arr[j] < arr[pos])
-            {
-                pos = j;
-            }
-        }
-        while (pos - i >= 2)
-        {
-            int temp = arr[pos - 2];
-            arr[pos - 2] = arr[pos];
-            arr[pos] = temp;
-            op.push_back({pos - 1, pos + 1});
-            pos -= 2;
-        }
-        if (pos - i == 1)
-        {
-            cout << "NO" << endl;
-            return 0;
-        }
-    }
-    for (int i = 1; i < n; i++)
-    {
-        if (arr[i] < arr[i - 1])
-        {
-            cout << "NO" << endl;
-            return 0;
-        }
-    }
     cout << "YES" << endl;
-    cout << op.size() << endl;
-    for (auto it : op)
-    {
-        cout << it.first << " " << it.second << endl;
+    cout << arr2.size() << endl;
+    for (int i = 0; i < arr2.size(); i++) {
+        cout << arr2[i] << " " << arr2[i] + 2 << endl;
     }
 }
--- H_Trains_2.cpp
+++ H_Trains_2.cpp
@@ -1,12 +1,103 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int n;
     cin >> n;
-    while(n--) {
-        
+    string train[n];
+    string destination[n];
+    string time[n];
+    int serial[n];
+    string word;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> train[i] >> word >> word >> word >> destination[i] >> word >> time[i];
+        serial[i] = i;
+    }
+    
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            bool swapped = false;
+            
+            if (train[i] != train[j])
+            {
+                int minimum = min(train[i].length(), train[j].length());
+                bool flag = false;
+
+                // char by char check
+                for (int k = 0; k < minimum; k++)
+                {
+                    char c1 = train[i][k];
+                    char c2 = train[j][k];
+                    if (c1 == c2)
+                        continue;
+
+                    if ((c1 >= 'a' && c1 <= 'z') && !(c2 >= 'a' && c2 <= 'z'))
+                    {
+                        flag = true;
+                        break;
+                    }
+                    else if (!(c1 >= 'a' && c1 <= 'z') && (c2 >= 'a' && c2 <= 'z'))
+                    {
+                        swapped = true;
+                        flag = true;
+                        break;
+                    }
+                    else
+                    {
+                        // same case
+                        if (c1 > c2)
+                            swapped = true;
+                        flag = true;
+                        break;
+                    }
+                }
+                
+                // Check prefix
+                if (!flag)
+                {
+                    if (train[i].length() > train[j].length())
+                        swapped = true;
+                }
+            }
+            else
+            {
+                int t1 = (time[i][0] - '0') * 10 + (time[i][1] - '0');
+                t1 = t1 * 60;
+                t1 += (time[i][3] - '0') * 10 + (time[i][4] - '0');
+
+                int t2 = (time[j][0] - '0') * 10 + (time[j][1] - '0');
+                t2 = t2 * 60;
+                t2 += (time[j][3] - '0') * 10 + (time[j][4] - '0');
+
+                if (t1 < t2)
+                    swapped = true;
+                else if (t1 == t2)  // If times equal, preserve input order
+                {
+                    if (serial[i] > serial[j])
+                        swapped = true;
+                }
+            }
+
+            if (swapped)
+            {
+                swap(train[i], train[j]);
+                swap(destination[i], destination[j]);
+                swap(time[i], time[j]);
+                swap(serial[i], serial[j]);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << train[i] << " will depart for "
+             << destination[i] << " at "
+             << time[i] << "\n";
     }
 }
